b06f831cb9830d8cfc61528e66cb7ba97c386022
Max Basov <62623726+scratchw0w@users.noreply.github.com>
2020-11-30 10:25:15 +0200

    added new app.js file

---
+++ js-let-test/app.js
@@ -0,0 +1,18 @@
+/*let name = 'Max';
+
+if (name === 'Max') {
+  let hobbies = ['Sports', 'Cooking'];
+  console.log(hobbies);
+
+}
+
+function greet() {
+  let age = 30;
+  let name = 'Manuel';
+  console.log(name, age, hobbies);
+}
+*/
+
+
+let userName = 'Serega';
+console.log(userName);
